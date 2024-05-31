52a57be60bf848917160192963dca8c68a86fb0e
atiracJ <167359352+atiracJ@users.noreply.github.com>
2024-05-31 15:16:52 -0300

    Add files via upload

---
+++ etapa1/07_Carita_AlReves/7_Carita_AlReves/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Carita_AlReves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string letra1;
+            string letra2;
+            string letra3;
+            Console.WriteLine("ingrese una letra ");
+            letra1 = Console.ReadLine();
+            Console.WriteLine("ingrese otra letra ");
+            letra2 = Console.ReadLine();
+            Console.WriteLine("ingrese una letra mas ");
+            letra3 = Console.ReadLine();
+
+            Console.WriteLine("dado vuelta seria ");
+            Console.WriteLine(letra3);
+            Console.WriteLine(letra2);
+            Console.WriteLine(letra1);
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
