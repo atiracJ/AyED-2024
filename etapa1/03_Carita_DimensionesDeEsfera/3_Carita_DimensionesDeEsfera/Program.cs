9e7dc4c6b60f2aa7320d827d20c495939715448f
atiracJ <167359352+atiracJ@users.noreply.github.com>
2024-05-31 15:08:29 -0300

    Add files via upload

---
+++ etapa1/03_Carita_DimensionesDeEsfera/3_Carita_DimensionesDeEsfera/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Carita_DimensionesDeEsfera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radio;
+            double superficie;
+            double volumen;
+            double pi = 3.14;
+            Console.WriteLine("ingresa el radio de la esfera: ");
+            radio = double.Parse(Console.ReadLine());
+
+
+            double radioCuadrado = radio * radio;
+            double radioCubo = radio * radio * radio;
+
+            superficie = 4 * pi * radioCuadrado;
+            volumen = (4.0 / 3.0) * pi * radioCubo;
+
+
+            Console.WriteLine("La superficie de la esfera es: " + superficie);
+            Console.WriteLine("El volumen de la esfera es: " + volumen));
+
+
+            Console.ReadKey();
+        }
+    }
+}
