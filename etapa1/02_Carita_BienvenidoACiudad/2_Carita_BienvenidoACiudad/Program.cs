060d49fda96ab310ac12681f0d33f45ec4e2f06c
atiracJ <167359352+atiracJ@users.noreply.github.com>
2024-05-31 15:02:31 -0300

    Add files via upload

---
+++ etapa1/02_Carita_BienvenidoACiudad/2_Carita_BienvenidoACiudad/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Carita_BienvenidoACiudad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ciudad;
+            string nombre;
+            Console.WriteLine("ingrese el nombre de una ciudad ciudad");
+            ciudad = Console.ReadLine();
+            Console.WriteLine("ingrese su nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre + " bienvenido a la ciudad de " + ciudad);
+            Console.ReadKey();
+        }
+    }
+}
