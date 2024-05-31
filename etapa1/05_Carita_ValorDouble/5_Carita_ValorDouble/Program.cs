a8397ccf1333d83e77fed20f8f10a89d1b88dd02
atiracJ <167359352+atiracJ@users.noreply.github.com>
2024-05-31 15:11:05 -0300

    Add files via upload

---
+++ etapa1/05_Carita_ValorDouble/5_Carita_ValorDouble/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Carita_ValorDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingrese el ancho del rectángulo:");
+            double ancho;
+            Console.ReadLine();
+
+            Console.WriteLine("Ingrese la altura del rectángulo:");
+            double altura;
+            Console.ReadLine();
+
+            double perimetro = 2 * (ancho + altura);
+
+            double area = ancho * altura;
+
+            double diagonal = Math.Sqrt(ancho * ancho + altura * altura);
+
+            Console.WriteLine("El perímetro del rectángulo es: " + perimetro);
+            Console.WriteLine("El área del rectángulo es: " + area);
+            Console.WriteLine("La diagonal del rectángulo es: " + diagonal);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
