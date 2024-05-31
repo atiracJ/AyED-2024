b1129a2b22dc889c7e733d4c97558b0dd11597c8
atiracJ <167359352+atiracJ@users.noreply.github.com>
2024-05-31 15:21:28 -0300

    Add files via upload

---
+++ etapa1/09_Carita_IngresePassword/9_Carita_IngresePassword/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Carita_IngresePassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string contraseña = "papucho" ;
+
+            Console.WriteLine("ingrese su contraseña");
+            string ContraseñaDeUsuario = Console.ReadLine();
+
+            if (ContraseñaDeUsuario == contraseña || ContraseñaDeUsuario == "papucho")
+            {
+                Console.WriteLine("La contra es correcta");
+            }
+             
+            
+            else
+            {
+                    Console.WriteLine("la contra es incorrecta");
+
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
