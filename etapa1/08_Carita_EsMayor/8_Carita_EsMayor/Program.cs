bea6ee9ce81655787f06103db1d9379051c4f30b
atiracJ <167359352+atiracJ@users.noreply.github.com>
2024-05-31 15:18:30 -0300

    Add files via upload

---
+++ etapa1/08_Carita_EsMayor/8_Carita_EsMayor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Carita_EsMayor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
