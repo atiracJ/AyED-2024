660c15a99ea04e1a6452c84ffc59b2ab259e3226
atiracJ <167359352+atiracJ@users.noreply.github.com>
2024-11-22 00:19:13 -0300

    Add files via upload

---
+++ etapa0/1_Carita_HolaNombre/1_Carita_HolaNombre/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Carita_HolaNombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre;
+            Console.WriteLine("ingrese su lindo nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre);
+            Console.ReadKey();
+        }
+    }
+}
