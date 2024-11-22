a99b78923b8e1011f011e6a85c2b5d6c3511d06d
atiracJ <167359352+atiracJ@users.noreply.github.com>
2024-11-22 15:15:49 -0300

    Add files via upload

---
+++ etapa1/14_Carita_Cumplidos/14_Carita_Cumplidos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Carita_Cumplidos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese su edad" );
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("has cumplido los siguientes años");
+            int contador = 1;
+            while (contador <= edad)
+            { Console.WriteLine(contador);
+                contador++;
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
