1de1f54ab40c829817dc8cc2a45735631b01e561
atiracJ <167359352+atiracJ@users.noreply.github.com>
2024-11-22 15:08:26 -0300

    Add files via upload

---
+++ etapa1/13_Carita_DiezVeces/13_Carita_DiezVeces/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Carita_DiezVeces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingresa una palabra");
+            string palabra = Console.ReadLine();
+
+            int contador = 0;
+            while (contador < 10)
+            { Console.WriteLine(palabra);
+                contador++;
+
+                Console.ReadKey();
+            } 
+
+
+
+
+
+
+        }
+    }
+}
