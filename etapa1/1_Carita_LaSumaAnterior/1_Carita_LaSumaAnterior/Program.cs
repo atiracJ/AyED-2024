859392b3feac3627b95e78c1cb33f9dd9dc9f1d8
atiracJ <167359352+atiracJ@users.noreply.github.com>
2024-11-22 00:20:05 -0300

    Add files via upload

---
+++ etapa1/1_Carita_LaSumaAnterior/1_Carita_LaSumaAnterior/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Carita_LaSumaAnterior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 10;
+            int numero2 = 20;
+            int numero3 = 30;
+
+            int suma = numero1 + numero2 + numero3;
+
+            Console.WriteLine("La suma de los números es: " + suma);
+
+            Console.ReadKey();
+        }
+    }
+}
