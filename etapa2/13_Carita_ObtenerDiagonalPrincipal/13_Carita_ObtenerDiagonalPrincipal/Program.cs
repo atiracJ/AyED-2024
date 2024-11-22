654f01f0dfc9897cdcb84ad14fa910ac883dcf76
atiracJ <167359352+atiracJ@users.noreply.github.com>
2024-11-21 23:04:44 -0300

    Add files via upload

---
+++ etapa2/13_Carita_ObtenerDiagonalPrincipal/13_Carita_ObtenerDiagonalPrincipal/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Carita_ObtenerDiagonalPrincipal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.Write("ingrese el tamaño de la matriz (n): ");
+                int n = int.Parse(Console.ReadLine());
+
+                int[,] matriz = new int[n, n];
+                Random random = new Random();
+
+                Console.WriteLine("\nMatriz generada con números aleatorios:");
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        matriz[i, j] = random.Next(1, 101); 
+                        Console.Write(matriz[i, j] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+
+                int[] diagonal = new int[n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    diagonal[i] = matriz[i, i];
+                }
+
+                Console.WriteLine("Diagonal principal de la matriz:");
+                for (int i = 0; i < diagonal.Length; i++)
+                {
+                    Console.Write(diagonal[i] + " ");
+                }
+
+                Console.WriteLine("Presione cualquier tecla para salir");
+                Console.ReadKey();
+            }
+        }
+    }
+
+
+}
