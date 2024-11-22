cde744014ddb1cc32dd4b299faadb7e755c5c6f6
atiracJ <167359352+atiracJ@users.noreply.github.com>
2024-11-21 23:30:48 -0300

    Add files via upload

---
+++ etapa3/00_Carita_MiPrimerFuncion/00_Carita_MiPrimerFuncion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_Carita_MiPrimerFuncion
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+                Console.Write("ingresa tu nombre: ");
+                string nombre = Console.ReadLine();
+
+                Mostrarsaludo(nombre);
+
+                Console.WriteLine("Presione cualquier tecla para salir");
+                Console.ReadKey();
+            }
+
+            static void Mostrarsaludo(string nombre)
+            {
+                Console.WriteLine($"¡Hola {nombre}!");
+            }
+        }
+    }
+
+
